ff1f8d559611a4b66482218b49ebfaad6378663e
Pol Quintana <pol.quintana1@gmail.com>
2021-12-21 12:21:55 +0100

    Add support for Carthage

--- fastlane/README.md
+++ fastlane/README.md
@@ -1,37 +1,43 @@
 fastlane documentation
-================
+----
+
 # Installation
 
 Make sure you have the latest version of the Xcode command line tools installed:
 
-```
+```sh
 xcode-select --install
 ```
 
-Install _fastlane_ using
-```
-[sudo] gem install fastlane -NV
-```
-or alternatively using `brew install fastlane`
+For _fastlane_ installation instructions, see [Installing _fastlane_](https://docs.fastlane.tools/#installing-fastlane)
 
 # Available Actions
+
 ### test_release
+
+```sh
+[bundle exec] fastlane test_release
 ```
-fastlane test_release
-```
+
 Start a new release
 
 ----
 
+
 ## iOS
+
 ### ios build_integration_app
+
+```sh
+[bundle exec] fastlane ios build_integration_app
 ```
-fastlane ios build_integration_app
-```
+
 Build Integration App
 
 ----
 
 This README.md is auto-generated and will be re-generated every time [_fastlane_](https://fastlane.tools) is run.
-More information about fastlane can be found on [fastlane.tools](https://fastlane.tools).
-The documentation of fastlane can be found on [docs.fastlane.tools](https://docs.fastlane.tools).
+
+More information about _fastlane_ can be found on [fastlane.tools](https://fastlane.tools).
+
+The documentation of _fastlane_ can be found on [docs.fastlane.tools](https://docs.fastlane.tools).
--- scripts/fetch_dependencies.sh
+++ scripts/fetch_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash -e
 
-# usage: ./scripts/fetch_dependencies.sh --integration=[integration] --version=[version]
+# usage: ./scripts/fetch_dependencies.sh --integration [integration] --version [version]
 #
 # Available options: "cocoapods", "spm"
 
@@ -26,37 +26,41 @@ do
 done
 
 function updatePods {
+  echo "> Grabbing SDKs using CocoaPods integration."
+  echo "> Running: pod update --repo-update"
   pod update --repo-update
 }
 
 function updateSPM {
+  echo "> Grabbing SDKs using SwiftPM integration. Version: $VERSION_NUMBER"
+  echo "> Running: bundle exec fastlane build_integration_app scheme:StreamChatIntegration-SPM"
+  
   # Updates version of StreamChatUI package in related StreamChatIntegration-SPMxcode project
   sed -i '' -e "s|version =.*|version = '$VERSION_NUMBER';|g" StreamChatIntegration-SPM.xcodeproj/project.pbxproj
   bundle exec fastlane build_integration_app scheme:StreamChatIntegration-SPM
 }
 
+function updateCarthage {
+  echo "> Grabbing SDKs using Carthage integration."
+  echo "> Running: carthage update --use-xcframeworks --no-use-binaries --platform iOS"
+  carthage update --use-xcframeworks --no-use-binaries --platform iOS
+}
+
 # CocoaPods
 if [[ "$INTEGRATION" = "cocoapods" ]]; then
-  echo "> Starting to grab SDKs using CocoaPods integration."
-  echo "> Running: pod update --repo-update"
   updatePods
   exit;
 fi
 
 # SPM
 if [[ "$INTEGRATION" = "spm" ]]; then
-  echo "> Starting to grab SDKs using SwiftPM integration. Version: $VERSION_NUMBER"
-  echo "> Running: bundle exec fastlane build_integration_app scheme:StreamChatIntegration-SPM"
   updateSPM
   exit;
 fi
 
 # Carthage
 if [[ "$INTEGRATION" = "carthage" ]]; then
-  echo "> Starting to grab SDKs using Carthage integration."
-
-  echo "> ./scripts/carthage.sh build --use-xcframeworks --platform iOS"
-  ./scripts/carthage.sh build --use-xcframeworks --platform iOS
+  updateCarthage
   exit;
 fi
 
@@ -64,12 +68,7 @@ fi
 if [[ -z "$INTEGRATION" ]]; then
   echo "> Integration option was not specified (eg: cocoapods or spm). Script will grab SDKs using all supported dependency managers."
 
-  echo "> Integration: cocoapods"
-  echo "> Running: pod update --repo-update"
   updatePods
-
-  echo "> Integration: SwiftPM"
-  echo "> Running: bundle exec fastlane build_integration_app scheme:StreamChatIntegration-SPM"
   updateSPM
-   
+  updateCarthage
 fi
